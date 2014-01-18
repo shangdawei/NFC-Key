d9c3b235dcc8260237347fbbe10de6e8dbecf3cd
mateusz-szafraniec <szafraniec.m.m@gmail.com>
2014-01-18 15:26:41 +0100

    License

--- src/pl/net/szafraniec/NFCKey/Settings.java
+++ src/pl/net/szafraniec/NFCKey/Settings.java
@@ -1,10 +1,10 @@
 package pl.net.szafraniec.NFCKey;
 
 public class Settings {
-	public static final String nfc_mime_type = "application/x-knfc";
+	public static final String nfc_mime_type = "application/x-nfck";
 	public static final String nfc_mime_type_hidden = "text/plain";
 	public static final String nfcinfo_filename_template = "nfcinfo";
-	public static final int password_length = 22; // including length byte oryginalnie 33 wyci¹gi 22(46), 113 NTAG203 (144)
+	public static final int password_length = 22; // including length byte original was 33, ultraligth 22, 113 NTAG203
 	public static final int index_length = 2; // Password filename number
 	public static final int config_length = 1; // Config byte, currently set if ask for password
 	public static final int random_bytes_length = password_length + config_length;
