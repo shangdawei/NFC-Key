15402470554635056d6202a075c32199ec8acc55
mateusz-szafraniec <szafraniec.m.m@gmail.com>
2014-01-18 16:02:51 +0100

    small changes

--- src/pl/net/szafraniec/NFCKey/AboutDialog.java
+++ src/pl/net/szafraniec/NFCKey/AboutDialog.java
@@ -30,11 +30,14 @@ public class AboutDialog extends Dialog{
 	public void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.about);
 		TextView tv = (TextView)findViewById(R.id.legal_text);
-		tv.setText(readRawTextFile(R.raw.legal));
+		//tv.setText(readRawTextFile(R.raw.legal));
+		tv.setText(Html.fromHtml(readRawTextFile(R.raw.legal)));
+		Linkify.addLinks(tv, Linkify.ALL);
 		tv = (TextView)findViewById(R.id.info_text);
 		tv.setText(Html.fromHtml(readRawTextFile(R.raw.info)));
 		tv.setLinkTextColor(Color.WHITE);
 		Linkify.addLinks(tv, Linkify.ALL);
+		
 	}
 	
 	public static String readRawTextFile(int id) {
